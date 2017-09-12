6004a55f4d9c67fa39c0d4de7e3c46a6c5c4b8e2
Thalles Batista <thallesmweb@gmail.com>
2017-09-12 14:30:17 +0000

    correções

--- libs/config.js
+++ libs/config.js
@@ -8,7 +8,12 @@ module.exports	= {
         host: "talentoses.cnflodg3fqfr.sa-east-1.rds.amazonaws.com",
         define:	{
             underscored: true
-        }
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        },
     },
     jwtSecret: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImp0aSI6IjdiYTllM2QxLTdiNDMtNDc1YS05NjVlLTYwOGNiNDdmMDUwOSIsImlhdCI6MTUwNDMwMDA0NCwiZXhwIjoxNTA0MzAzNjQ0fQ.SvOQ6obzHp-GMstXjeSusgQdzLhPEXgFAZzOldVqlZA",
     jwtSession:	{ session: false }
