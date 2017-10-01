5378235efb92e0884890b8b3b4e458eea580b794
Nathalia Simmer <nathaliasimmer@hotmail.com>
2017-10-01 14:04:04 +0000

    Alterações no endpoint de formação acadêmica

--- routes/formacaoAcademica.js
+++ routes/formacaoAcademica.js
@@ -2,52 +2,73 @@ module.exports = app => {
     const FormacaoAcademica = app.db.models.FormacoesAcademicas;
 
     app.route("/formacaoacademica")
-    .get((req, res) => {
-             FormacaoAcademica.findAll().then(result => res.json(result))
-            .catch(error => {
-                 res.status(412).json({msg: error.message});
-             });
-         })
-         .post((req, res) => {
-             FormacaoAcademica.create(req.body)
-             .then(result => res.json(result))
-             .catch(error => {
-                 res.status(412).json({msg: error.message});
-             });
-         });
-         
-     app.route("/formacaoacademica/:id")
-         .get((req, res) => {
-             FormacaoAcademica.findOne({
-                 where: {id: req.params.id}
-             }).then(result => res.json(result))
-             .catch(error => {
-                 res.status(412).json({msg: error.message});
-             });
-         })
-         .put((req, res) => {
-             FormacaoAcademica.update(
-                  {
-                     curso: req.body.curso,
-                     dataInicio: req.body.dataInicio,
-                     sexo: req.body.sexo,
-                     dataFim: req.body.dataFim,
-                     nivel: req.body.nivel
-                  },
-                  {where: { id: req.params.id }}
-             ).then(result => res.json(result))
-                 .catch(error => {
-                     res.status(412).json({msg: error.message});
-             });
-         })
-         .delete((req, res) => {
-             FormacaoAcademica.destroy({
-                 where: {
-                     id: req.params.id
-                 }
-             }).then(result => res.json(result))
-                 .catch(error => {
-                     res.status(412).json({msg: error.message});
-             });
-         });
+        .post((req, res) => {
+            console.log(req.body);
+            FormacaoAcademica.create(req.body)
+                .then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({
+                        msg: error.message
+                    });
+                });
+        });
+
+    app.route("/formacaoacademica/:id")
+        .get((req, res) => {
+            FormacaoAcademica.findOne({
+                    where: {
+                        id: req.params.id
+                    }
+                }).then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({
+                        msg: error.message
+                    });
+                });
+        })
+        .delete((req, res) => {
+            FormacaoAcademica.destroy({
+                    where: {
+                        id: req.params.id
+                    }
+                }).then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({
+                        msg: error.message
+                    });
+                });
+        })
+        .put((req, res) => {
+            FormacaoAcademica.update({
+                    curso: req.body.curso,
+                    dataInicio: req.body.dataInicio,
+                    dataFim: req.body.dataFim,
+                    nivel: req.body.nivel,
+                    instituicoes_academica_id: req.body.instituicoes_academica_id
+                }, {
+                    where: {
+                        id: req.params.id
+                    }
+                }).then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({
+                        msg: error.message
+                    });
+                });
+        });
+
+    app.route("/servidores/:idServidor/formacaoacademica")
+        .get((req, res) => {
+            FormacaoAcademica.findAll({
+                    where: {
+                        servidore_id: req.params.idServidor
+                    }
+                }).then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({
+                        msg: error.message
+                    });
+                });
+        });
+
 };
