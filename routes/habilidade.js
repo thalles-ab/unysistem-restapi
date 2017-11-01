9c847e633663f42c96855e3877f69196cc5c4f95
SirSalute <acetequila@gmail.com>
2017-11-01 01:52:50 +0000

    Alteração para funcionamento de autocomplete de habilidade

--- routes/habilidade.js
+++ routes/habilidade.js
@@ -56,4 +56,24 @@ module.exports = app => {
                     res.status(412).json({ msg: error.message });
                 });
         });
+        
+        app.route("/habilidade/nome/:nome")
+        .get((req, res) => {
+            Habilidade.findAll({
+                attributes: ['id', 'nome'],
+                where: {
+                    nome: {
+                        $like: "%" + req.params.nome + "%"
+                    }
+                },
+                limit: 10,
+                order: [
+                    ['nome', 'ASC']
+                ]
+            })
+                .then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({ msg: error.message });
+                });
+        });
 };
