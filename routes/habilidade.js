bf348dc6fc421d0202cb98701f4feccb9f12f535
Nathalia Simmer <nathaliasimmer@hotmail.com>
2017-11-26 10:38:40 +0000

    Evitando duplicidade de habilidade

--- routes/habilidade.js
+++ routes/habilidade.js
@@ -4,10 +4,10 @@ module.exports = app => {
     app.route("/servidores/:idServidor/habilidade")
         .get((req, res) => {
             Habilidade.findAll({
-                where: {
-                    servidor_id: req.params.idServidor
-                }
-            }).then(result => res.json(result))
+                    where: {
+                        servidor_id: req.params.idServidor
+                    }
+                }).then(result => res.json(result))
                 .catch(error => {
                     res.status(412).json({
                         msg: error.message
@@ -17,7 +17,90 @@ module.exports = app => {
 
     app.route("/habilidade")
         .post((req, res) => {
-            Habilidade.create(req.body)
+            Habilidade.findOne({
+                where: {
+                    servidor_id: req.body.servidor_id,
+                    nome: {
+                        $like: req.body.nome
+                    }
+                }
+            }).then(result => {
+                if (result) {
+                    res.status(412).json({
+                        msg: "Habilidade já cadastrada."
+                    });
+                } else {
+                    Habilidade.create(req.body)
+                        .then(result => res.json(result))
+                        .catch(error => {
+                            res.status(412).json({
+                                msg: error.message
+                            });
+                        });
+                }
+            }).catch(error => {
+                res.status(412).json({
+                    msg: error.message
+                });
+            });
+
+
+        });
+
+    app.route("/habilidade/:id")
+        .get((req, res) => {
+            Habilidade.findOne({
+                    where: {
+                        id: req.params.id
+                    }
+                }).then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({
+                        msg: error.message
+                    });
+                });
+        })
+        .put((req, res) => {
+            Habilidade.update({
+                    nome: req.body.nome
+                }, {
+                    where: {
+                        id: req.params.id
+                    }
+                }).then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({
+                        msg: error.message
+                    });
+                });
+        })
+        .delete((req, res) => {
+            Habilidade.destroy({
+                    where: {
+                        id: req.params.id
+                    }
+                }).then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({
+                        msg: error.message
+                    });
+                });
+        });
+
+    app.route("/habilidade/nome/:nome")
+        .get((req, res) => {
+            Habilidade.findAll({
+                    attributes: ['id', 'nome'],
+                    where: {
+                        nome: {
+                            $like: "%" + req.params.nome + "%"
+                        }
+                    },
+                    limit: 10,
+                    order: [
+                        ['nome', 'ASC']
+                    ]
+                })
                 .then(result => res.json(result))
                 .catch(error => {
                     res.status(412).json({
@@ -26,59 +109,8 @@ module.exports = app => {
                 });
         });
 
-    app.route("/habilidade/:id")
-        .get((req, res) => {
-            Habilidade.findOne({
-                where: { id: req.params.id }
-            }).then(result => res.json(result))
-                .catch(error => {
-                    res.status(412).json({ msg: error.message });
-                });
-        })
-        .put((req, res) => {
-            Habilidade.update(
-                {
-                    nome: req.body.nome
-                },
-                { where: { id: req.params.id } }
-            ).then(result => res.json(result))
-                .catch(error => {
-                    res.status(412).json({ msg: error.message });
-                });
-        })
-        .delete((req, res) => {
-            Habilidade.destroy({
-                where: {
-                    id: req.params.id
-                }
-            }).then(result => res.json(result))
-                .catch(error => {
-                    res.status(412).json({ msg: error.message });
-                });
-        });
-        
-        app.route("/habilidade/nome/:nome")
-        .get((req, res) => {
-            Habilidade.findAll({
-                attributes: ['id', 'nome'],
-                where: {
-                    nome: {
-                        $like: "%" + req.params.nome + "%"
-                    }
-                },
-                limit: 10,
-                order: [
-                    ['nome', 'ASC']
-                ]
-            })
-                .then(result => res.json(result))
-                .catch(error => {
-                    res.status(412).json({ msg: error.message });
-                });
-        });
 
-
-        app.route("/habilidade")
+    app.route("/habilidade")
         .get((req, res) => {
             app.routes.autoSearch(Habilidade, req.query)
                 .then(result => res.json(result));
